49a310e86d537c012d96c24237c0c25eb4df7e91
Krzysztof Porwol <220742689+KrzysiekPorwol@users.noreply.github.com>
2026-09-09 11:02:44 +0200

    Struktury danych - Tablice zadanie 1

--- src/AppLauncher/App.java
+++ src/AppLauncher/App.java
@@ -1,16 +1,27 @@
 package AppLauncher;
 
-import model.Car;
-
 public class App {
 
   static void main() {
 
-    Car car = new Car("blue", "Mustang", 2.5);
+    int[] numbers = new int[5];
+    int sum = 0;
 
-    Car car1 = new Car();
+    for (int i = 0; i < numbers.length; i++) {
+      numbers[i] = i + 1;
+    }
 
-    car.showInfo();
+    for (int i = 0; i < numbers.length; i++) {
+      System.out.println("Element o indeksie " + i + " ma wartość " + numbers[i]);
+      sum = sum + numbers[i];
+    }
+
+    System.out.println("Suma wszyskich elementów: " + sum);
   }
 
 }
+
+
+//Tworzy tablicę int z pięcioma dowolnymi liczbami.
+//Przechodzi po niej pętlą i wypisuje każdy element w formacie: Element o indeksie 0 ma wartosc 12.
+//Na końcu wypisuje sumę wszystkich elementów.
